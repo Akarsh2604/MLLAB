9a4d7325e2f1110e141210b01303688ab47e8dea
Akarsh2604 <147085525+Akarsh2604@users.noreply.github.com>
2024-08-04 13:29:02 +0530

    Update lab3code3.py

--- lab3code3.py
+++ lab3code3.py
@@ -1,3 +1,3 @@
 #A3
-buying_data['status'] = buying_data['Payment (Rs)'].apply(lambda x: "RICH" if x > 200 else "POOR")
-print(buying_data[['Payment (Rs)', 'status']].head())
+buy_data['status'] = buy_data['Payment (Rs)'].apply(lambda x: "RICH" if x > 200 else "POOR")
+print(buy_data[['Payment (Rs)', 'status']].head())
